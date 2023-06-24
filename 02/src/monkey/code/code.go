cab06cf7f69cc460467a8e62f528097f4714daa2
sawyerrick <1103031078@qq.com>
2023-06-25 00:13:34 +0800

    smallest compiler

--- 02/src/monkey/code/code.go
+++ 02/src/monkey/code/code.go
@@ -1,16 +1,52 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
+// Instructions is consisted of many opcodes and its operands
 type Instructions []byte
 
+// print current position of a instruction and its counterpart opcode definitions
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+		operands, read := ReadOperands(def, ins[i+1:])
+        // i is the starting-position of an instruction
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+		i += 1 + read
+	}
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n",
+			len(operands), operandCount)
+	}
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
+
+// Opcode: like a function
+// Operand: like a function's arguments
 type Opcode byte
 
 const (
 	// start from 0, increase by byte
+    // one byte for each opcode
 	OpConstant Opcode = iota
 )
 
@@ -33,6 +69,9 @@ func Lookup(op byte) (*Definition, error) {
 	return def, nil
 }
 
+// Encodes the opcode and its operands into bytecode instruction
+// operands now is a reference to constant pool
+// aka assembler
 func Make(op Opcode, operands ...int) []byte {
 	def, ok := definitions[op]
 	if !ok {
@@ -53,5 +92,26 @@ func Make(op Opcode, operands ...int) []byte {
 		}
 		offset += width
 	}
+
 	return instruction
+}
+
+// The couterpart of Make
+// Decode bytecode instruction into operands
+// aka disassembler
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return operands, offset
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
--- 02/src/monkey/code/code_test.go
+++ 02/src/monkey/code/code_test.go
@@ -1,6 +1,9 @@
 package code
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMake(t *testing.T) {
 	tests := []struct {
@@ -11,7 +14,7 @@ func TestMake(t *testing.T) {
 		{
 			OpConstant,
 			[]int{65534},
-			[]byte{ byte(OpConstant), 255, 254 }, // 65534 big endian
+			[]byte{byte(OpConstant), 255, 254}, // 65534 big endian
 		},
 	}
 	for _, tt := range tests {
@@ -24,6 +27,61 @@ func TestMake(t *testing.T) {
 			if instruction[i] != tt.expected[i] {
 				t.Errorf("wrong byte at pos %d. want=%d, got=%d",
 					i, b, instruction[i])
+			}
+		}
+	}
+}
+
+// fmt see: https://pkg.go.dev/fmt
+func TestPrintf(t *testing.T) {
+    bytecodes := Make(OpConstant, 65535)
+
+    fmt.Printf("%4b \n", bytecodes)
+    fmt.Println((Instructions(bytecodes)))
+}
+
+func TestInstructionsString(t *testing.T) {
+	instructions := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+    // bytecode slice
+	concatted := Instructions{}
+	for _, ins := range instructions {
+		concatted = append(concatted, ins...)
+	}
+	if concatted.String() != expected {
+		t.Errorf("instructions wrongly formatted.\nwant=%q\ngot=%q",
+			expected, concatted.String())
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{OpConstant, []int{65535}, 2},
+	}
+	for _, tt := range tests {
+		instruction := Make(tt.op, tt.operands...)
+		def, err := Lookup(byte(tt.op))
+		if err != nil {
+			t.Fatalf("definition not found: %q\n", err)
+		}
+		operandsRead, n := ReadOperands(def, instruction[1:])
+		if n != tt.bytesRead {
+			t.Fatalf("n wrong. want=%d, got=%d", tt.bytesRead, n)
+		}
+		for i, want := range tt.operands {
+			if operandsRead[i] != want {
+				t.Errorf("operand wrong. want=%d, got=%d", want, operandsRead[i])
 			}
 		}
 	}
