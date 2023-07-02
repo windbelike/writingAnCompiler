ebd1166c92cb603d0d2d8ec420b94330ca5180f5
sawyerrick <1103031078@qq.com>
2023-07-02 11:55:11 +0800

    hash

--- 06/src/monkey/code/code.go
+++ 06/src/monkey/code/code.go
@@ -37,6 +37,7 @@ const (
 	OpGetGlobal
 	OpSetGlobal
 	OpArray
+	OpHash
 )
 
 // definition for opcode
@@ -66,6 +67,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
 }
 
 // loop up opcode definition
--- 06/src/monkey/vm/vm.go
+++ 06/src/monkey/vm/vm.go
@@ -28,11 +28,14 @@ type VM struct {
 
 func New(bytecode *compiler.Bytecode) *VM {
 	return &VM{
+		// same as Compiler
 		instructions: bytecode.Instructions,
 		constants:    bytecode.Constants,
-		stack:        make([]object.Object, StackSize),
-		sp:           0,
-		globals:      make([]object.Object, GlobalsSize),
+		// VM only
+		stack: make([]object.Object, StackSize),
+		sp:    0,
+		// for Identifier binding
+		globals: make([]object.Object, GlobalsSize),
 	}
 }
 
@@ -127,6 +130,19 @@ func (vm *VM) Run() error {
 			array := vm.buildArray(vm.sp-numElements, vm.sp)
 			vm.sp = vm.sp - numElements
 			err := vm.push(array)
+			if err != nil {
+				return err
+			}
+
+		case code.OpHash:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			hash, err := vm.buildHash(vm.sp-numElements, vm.sp)
+			if err != nil {
+				return err
+			}
+			vm.sp = vm.sp - numElements
+			err = vm.push(hash)
 			if err != nil {
 				return err
 			}
@@ -291,4 +307,19 @@ func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
 		elements[i-startIndex] = vm.stack[i]
 	}
 	return &object.Array{Elements: elements}
+}
+
+func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error) {
+	hashedPairs := make(map[object.HashKey]object.HashPair)
+	for i := startIndex; i < endIndex; i += 2 {
+		key := vm.stack[i]
+		value := vm.stack[i+1]
+		pair := object.HashPair{Key: key, Value: value}
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return nil, fmt.Errorf("unusable as hash key: %s", key.Type())
+		}
+		hashedPairs[hashKey.HashKey()] = pair
+	}
+	return &object.Hash{Pairs: hashedPairs}, nil
 }
