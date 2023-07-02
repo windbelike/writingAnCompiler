e1233047f5b494f821857d6b5c52eb0e7fd449b4
sawyerrick <1103031078@qq.com>
2023-07-02 13:25:35 +0800

    07 init

---
+++ 07/go.mod
@@ -0,0 +1,3 @@
+module sawyer.com/v7
+
+go 1.18
---
+++ 07/src/monkey/repl/repl.go
@@ -0,0 +1,81 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"sawyer.com/v7/src/monkey/compiler"
+	"sawyer.com/v7/src/monkey/lexer"
+	"sawyer.com/v7/src/monkey/object"
+	"sawyer.com/v7/src/monkey/parser"
+	"sawyer.com/v7/src/monkey/vm"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	// slices are pointer/reference type
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		comp := compiler.NewWithState(symbolTable, constants)
+		err := comp.Compile(program)
+		// fmt.Printf("constants: %v\n", comp.Bytecode().Constants)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
+		}
+		constants = comp.Bytecode().Constants
+		machine := vm.NewWithGlobalsStore(comp.Bytecode(), globals)
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+		// stackTop := machine.StackTop()
+		// io.WriteString(out, stackTop.Inspect())
+		// io.WriteString(out, "\n")
+		lastPopped := machine.LastPoppedStackElem()
+		io.WriteString(out, lastPopped.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
