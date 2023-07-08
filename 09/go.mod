e0680a8ead95b11dbe7557aa069f6b7ac3c4194a
sawyerrick <1103031078@qq.com>
2023-07-08 15:28:06 +0800

    init 09 closures

---
+++ 09/go.mod
@@ -0,0 +1,3 @@
+module sawyer.com/v9
+
+go 1.18
---
+++ 09/src/monkey/compiler/symbol_table.go
@@ -0,0 +1,62 @@
+package compiler
+
+type SymbolScope string
+
+const (
+	GlobalScope  SymbolScope = "GLOBAL"
+	LocalScope   SymbolScope = "LOCAL"
+	BuiltinScope SymbolScope = "BUILTIN"
+)
+
+type Symbol struct {
+	Name  string
+	Scope SymbolScope
+	Index int // symbol operand, used for setGlobal, getGlobal
+}
+
+// recursive SymbolTable, similar to  recursive environment in interpreter
+type SymbolTable struct {
+	Outer *SymbolTable
+	// symbol name to symbol mapping
+	store          map[string]Symbol
+	numDefinitions int
+}
+
+func NewSymbolTable() *SymbolTable {
+	s := make(map[string]Symbol)
+	return &SymbolTable{store: s}
+}
+
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	s := NewSymbolTable()
+	s.Outer = outer
+	return s
+}
+
+func (s *SymbolTable) Define(name string) Symbol {
+	symbol := Symbol{Name: name, Index: s.numDefinitions}
+	if s.Outer == nil {
+		symbol.Scope = GlobalScope
+	} else {
+		symbol.Scope = LocalScope
+	}
+	s.store[name] = symbol
+	s.numDefinitions++
+
+	return symbol
+}
+
+func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
+	obj, ok := s.store[name]
+	if !ok && s.Outer != nil {
+		obj, ok = s.Outer.Resolve(name)
+		return obj, ok
+	}
+	return obj, ok
+}
+
+func (s *SymbolTable) DefineBuiltin(index int, name string) Symbol {
+	symbol := Symbol{Name: name, Index: index, Scope: BuiltinScope}
+	s.store[name] = symbol
+	return symbol
+}
